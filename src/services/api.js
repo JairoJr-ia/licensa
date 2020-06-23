41c34c7c2c1629ec3c994fe58a1f32df5e589f09
JairoJr-ia <jairo_juniorjj@hotmail.com>
2020-06-23 19:24:42 -0300

    Alterado porta

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 console.log(process.env.BANCO_URL)
-const api = axios.create({ baseURL : process.env.baseURL || "http://191.37.47.54:9080" });
+const api = axios.create({ baseURL : process.env.baseURL || "https://191.37.47.54:9080" });
 export default api;
